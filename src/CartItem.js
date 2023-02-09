9ae75e181406be85db51ae01a4f3fe404b6b04c2
RohitSaini11 <rs0118032@gmail.com>
2023-02-09 23:50:43 +0530

    Added State

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,18 +2,31 @@ import React from "react";
 
 class CartItem extends React.Component{ //inherits properties from React.component
 
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render(){
+        const { price , title , qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize:25} }>Phone</div>
-                    <div style={ {color:'#777'} }>Rs 999</div>
-                    <div style={ {color:'#777'} }>Qty: 1</div>
+                    <div style={ {fontSize:25} }>{title}</div>
+                    <div style={ {color:'#777'} }>Rs:{price}</div>
+                    <div style={ {color:'#777'} }>Qty:{qty}</div>
                     <div className="cart-item-actions">
                         {/* BUTTONS */}
+                        <img alt="increase" className="action-icons" src="plus.png"/>
+                        <img alt="decrease" className="action-icons" src="minus.png"/>
+                        <img alt="delete" className="action-icons" src="delete.png"/>
                     </div>
                 </div>
             </div>
